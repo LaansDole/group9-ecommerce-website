eac0401144781b594ee77faf8d7b8f022dc157f3
Do Le Long An <85084360+LaansDole@users.noreply.github.com>
2023-05-05 05:11:27 +0000

    add errors

--- models/user.js
+++ models/user.js
@@ -1,22 +1,53 @@
-const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose');
-const Product = require('./product');
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+	{
+        name:{
+            type:String,
+            required:true,
+        },
 
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    cart: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref:'Product'
-        }
-    ]
+		username: {
+			type: String,
+			required: [true, "Please provide username"],
+			minlength: [5, "Length must be greater than 5"],
+			maxlength: [22, "Length must be less than 22"],
+			trim: true,
+			unique: true,
+		},
 
+		password: {
+			type: String,
+			required: true,
+		},
+
+		avatar: {
+			type: String,
+			required: true,
+		},
+
+		role: {
+            type: String,
+            default: 'user',
+        },
+
+
+        
+	},
+	{ timestamps: true }
+);
+
+UserSchema.pre("save", async function () {
+	if (!this.isModified("password")) return;
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
 });
 
-userSchema.plugin(passportLocalMongoose);
-module.exports = mongoose.model('User', userSchema);
+UserSchema.methods.comparePassword = async function (canditatePassword) {
+	const isMatch = await bcrypt.compare(canditatePassword, this.password);
+	return isMatch;
+};
+
+module.exports = mongoose.model("User", UserSchema);
