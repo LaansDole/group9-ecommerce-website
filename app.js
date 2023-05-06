dd7d3e4fd173ff5e08874a9b7462ab9fb699f16d
dolelongan <dolelongan@gmail.com>
2023-05-06 10:59:07 +0700

    add tmp holder for file

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const fileUpload = require('express-fileupload');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -14,14 +15,14 @@ app.use(express.urlencoded( { extended: true } ));
 app.use(express.static('public'));
 app.use(expressLayouts);
 
-app.use(cookieParser('CookingBlogSecure'));
+app.use(cookieParser('eCommerceSecure'));
 app.use(session({
-  secret: 'CookingBlogSecretSession',
+  secret: 'eCommerceSecretSession',
   saveUninitialized: true,
   resave: true
 }));
 app.use(flash());
-app.use(fileUpload());
+app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" }));
 
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
