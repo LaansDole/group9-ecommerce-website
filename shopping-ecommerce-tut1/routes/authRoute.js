52216ffed19100c41f4b90d3940962315900b584
Stephenlao <114713690+Stephenlao@users.noreply.github.com>
2023-05-10 09:48:01 +0700

    Delete shopping-ecommerce-tut1 directory

--- shopping-ecommerce-tut1/routes/authRoute.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const createUser = require("../controller/userCtrl");
-const router = express.Router();
-router.post("/register",createUser);
-module.exports = router;
