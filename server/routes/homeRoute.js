392399c0957818c3b74e1315f450400a94b31a67
dolelongan <dolelongan@gmail.com>
2023-05-13 14:26:10 +0700

    fix authentication in homepage

--- server/routes/homeRoute.js
+++ server/routes/homeRoute.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/homeController');
+const { authMiddleware, checkVendorRole, checkCustomerRole, checkShipperRole } = require("../../middlewares/authMiddleware");
 
 /**
  * App Routes 
 */
-router.get('/', homeController.homepage);
-router.get('/product/:id', homeController.exploreProduct);
-router.get('/categories', homeController.exploreCategories);
-router.get('/categories/:id', homeController.exploreCategoriesById);
-router.post('/search', homeController.searchProduct);
-router.get('/explore-latest', homeController.exploreLatest);
+router.get('/', authMiddleware, checkCustomerRole, homeController.homepage);
+router.get('/product/:id', authMiddleware, checkCustomerRole, homeController.exploreProduct);
+router.get('/categories', authMiddleware, checkCustomerRole, homeController.exploreCategories);
+router.get('/categories/:id', authMiddleware, checkCustomerRole, homeController.exploreCategoriesById);
+router.post('/search', authMiddleware, checkCustomerRole, homeController.searchProduct);
+router.get('/explore-latest', authMiddleware, checkCustomerRole, homeController.exploreLatest);
 // router.get('/submit-product', homeController.submitProduct);
 // router.post('/submit-product', homeController.submitProductOnPost);
 
