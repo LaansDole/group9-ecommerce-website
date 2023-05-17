f8d9a8ac3630d4e333df48d17e0da7262fa5f87d
vntuananh <vntuananhbui@gmail.com>
2023-05-17 18:43:46 +0700

    update multer and fileupload conflic

--- server/controllers/vendorController.js
+++ server/controllers/vendorController.js
@@ -35,48 +35,71 @@ const createProduct = async (req, res) => {
   res.render('submit-product', { title: 'E-Commerce - Submit Product', infoErrorsObj, infoSubmitObj, layout: './layouts/homeLayout' });
 };
 
+const multer = require('multer');
+
+// Set up multer storage and file filter
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/uploads/');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  }
+});
+
+const fileFilter = function (req, file, cb) {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(new Error('Only image files with extensions .jpeg or .png are allowed.'), false);
+  }
+};
+
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
+// Route handler for creating a product
 const createProductpost = async (req, res) => {
   try {
+    upload.single('image')(req, res, async function (err) {
+      if (err instanceof multer.MulterError) {
+        // A Multer error occurred during file upload
+        console.log(err);
+        return res.status(400).json({ error: 'Error uploading file.' });
+      } else if (err) {
+        // An unknown error occurred during file upload
+        console.log(err);
+        return res.status(500).json({ error: 'Internal server error.' });
+      }
 
-    let imageUploadFile;
-    let uploadPath;
-    let newImageName;
+      // File upload successful
+      const { name, description, price, productNotes, category } = req.body;
 
-    if (!req.files || Object.keys(req.files).length === 0) {
-      console.log('No Files where uploaded.');
-    } else {
+      // Get the uploaded file name
+      const imageName = req.file.filename;
 
-      imageUploadFile = req.files.image;
-      newImageName = Date.now() + imageUploadFile.name;
+      // Create a new product with the uploaded file name
+      const newProduct = new Product({
+        v_id: req.user._id,
+        name: name,
+        description: description,
+        price: price,
+        productNotes: productNotes,
+        category: category,
+        image: imageName
+      });
 
-      uploadPath = require('path').resolve('./') + '/public/uploads/' + newImageName;
+      await newProduct.save();
 
-      imageUploadFile.mv(uploadPath, function (err) {
-        if (err) return res.satus(500).send(err);
-      })
-
-    }
-
-    const newProduct = new Product({
-      v_id: req.user._id,
-      name: req.body.name,
-      description: req.body.description,
-      price: req.body.price,
-      productNotes: req.body.productNotes,
-      category: req.body.category,
-      image: newImageName
+      req.flash('infoSubmit', 'Product has been added.');
+      res.redirect('/vendor/submit-product');
     });
-
-    await newProduct.save();
-
-    req.flash('infoSubmit', 'Product has been added.')
-    res.redirect('/vendor/submit-product');
   } catch (error) {
-    // res.json(error);
-    req.flash('infoErrors', error)
+    req.flash('infoErrors', error);
     res.redirect('/vendor/submit-product');
   }
-}
+};
+
 
 
 const deleteProductform = async (req, res) => {
