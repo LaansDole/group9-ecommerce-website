b6663a7b5e6ea3aa662c27dc7b6ca7191191ae9c
dolelongan <dolelongan@gmail.com>
2023-05-18 22:58:40 +0700

    small fix

--- server/model/confirmOrderModel.js
+++ server/model/confirmOrderModel.js
@@ -1,47 +1,47 @@
 const mongoose = require('mongoose');
 const confirmOrderSchema = new mongoose.Schema({
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+
+  },
+  products: [{
+    name: {
+      type: String,
+      required: true
     },
-    products: [{
-      name: {
-        type: String,
-        required: true
-      },
-      price: {
-        type: Number,
-        required: true
-      },
-      quantity: {
-        type: Number,
-        required: true
-      }
-    }],
-    totalPrice: {
+    price: {
       type: Number,
       required: true
     },
-    orderStatus: {
-      type: String,
-      enum: ['active', 'delivered', 'cancel'],
-      default: 'active'
-    },
-    hubName: {
-      type: String,
-      enum: ['GHTK', 'viettel', 'ghn','aha'],
-      required: true
-    },
-    customerName: {
-      type: String,
-      required: true
-    },
-    customerAddress: {
-      type: String,
+    quantity: {
+      type: Number,
       required: true
     }
-  }, {
-    timestamps: true
-  });
+  }],
+  totalPrice: {
+    type: Number,
+    required: true
+  },
+  orderStatus: {
+    type: String,
+    enum: ['active', 'delivered', 'cancel'],
+    default: 'active'
+  },
+  hubName: {
+    type: String,
+    enum: ['GHTK', 'viettel', 'ghn', 'aha-move'],
+    required: true
+  },
+  customerName: {
+    type: String,
+    required: true
+  },
+  customerAddress: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
 module.exports = mongoose.model('confirmOrder', confirmOrderSchema);
