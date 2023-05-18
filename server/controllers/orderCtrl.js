b1ca6b399538247b3cedbad6b3ab2b69e3b0b628
vntuananh <vntuananhbui@gmail.com>
2023-05-18 13:33:10 +0700

    update route order

--- server/controllers/orderCtrl.js
+++ server/controllers/orderCtrl.js
@@ -1,75 +1,47 @@
 const asyncHandler = require('express-async-handler');
-const Order = require('../model/orderModel');
+const confirmOrder = require('../model/confirmOrderModel');
 
-// const createOrder = asyncHandler(async (req, res) => {
-//   const { hubName } = req.body;
-//   const products = [];
 
-//   for (let i = 0; i < 4; i++) {
-//     if (req.body[`products_${i}`]) {
-//       products.push({
-//         product_Name: req.body[`product_${i}`],
-//         product_Price: req.body[`product_Price_${i}`],
-//         businessName: req.body[`product_businessName_${i}`]
-//       });
-//     }
-//   }
-
-//   const order = await Order.create({
-//     products,
-//     hubName
-//   });
-
-//   res.status(201).json({ success: true, order });
-// });
 const express = require('express');
 const router = express.Router();
-// const Order = require('../models/order');
 
-const createOrder = ('/ordersubmit', async (req, res) => {
+
+const createOrder = async (req, res) => {
   try {
-    const orderData = req.body;
+    const { products, customerName, customerAddress, hubDelivery } = req.body;
+    const { name, address } = req.user;
 
-    // Create a new order instance
-    const order = new Order({
-      products: orderData.products,
-      customerName: orderData.customerName,
-      customerAddress: orderData.customerAddress,
+    const totalPrice = Object.values(products).reduce((sum, product) => {
+      return sum + product.price * product.quantity;
+    }, 0);
+
+    const order = new confirmOrder({
+      user: req.user._id,
+      products: Object.values(products),
+      totalPrice,
       orderStatus: 'active',
-      hubName: orderData.hubDelivery,
+      hubName: hubDelivery,
+      customerName: customerName || name,
+      customerAddress: customerAddress || address
     });
 
-    // Save the order to the database
     await order.save();
 
     res.status(200).json({ message: 'Order submitted successfully' });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: 'Failed to submit order' });
   }
-});
+};
 
-const submitOrder = async (req, res) => {
+const submitOrder =  async (req, res) => {
   try {
-    const orderData = req.body;
+    const { user } = req;
+    const orderHistory = await confirmOrder.find({ user: user._id });
 
-    // Create a new order instance
-    const order = new Order({
-      products: orderData.products,
-      customerName: orderData.customerName,
-      customerAddress: orderData.customerAddress,
-      orderStatus: 'active',
-      hubName: orderData.hubDelivery,
-    });
-
-    // Save the order to the database
-    await order.save();
-
-    res.status(200).json({ message: 'Order submitted successfully' });
-    
+    res.render('home-page/orderHistory', { orderHistory: orderHistory || [] });
   } catch (error) {
-    
-    res.status(500).json({ error: 'Failed to submit order' });
-    console.log(error)
+    res.status(500).json({ error: 'Failed to retrieve order history' });
   }
 };
 
--- server/routes/orderRouter.js
+++ server/routes/orderRouter.js
@@ -7,46 +7,11 @@ const confirmOrder = require('../model/confirmOrderModel');
 const router = express.Router();
 
 // submit order
-router.post('/submit', authMiddleware, checkCustomerRole, async (req, res) => {
-  try {
-    const { products, customerName, customerAddress, hubDelivery } = req.body;
-    const { name, address } = req.user;
-
-    const totalPrice = Object.values(products).reduce((sum, product) => {
-      return sum + product.price * product.quantity;
-    }, 0);
-
-    const order = new confirmOrder({
-      user: req.user._id,
-      products: Object.values(products),
-      totalPrice,
-      orderStatus: 'active',
-      hubName: hubDelivery,
-      customerName: customerName || name,
-      customerAddress: customerAddress || address
-    });
-
-    await order.save();
-
-    res.status(200).json({ message: 'Order submitted successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to submit order' });
-  }
-});
+router.post('/submit', authMiddleware, checkCustomerRole, createOrder)
 
 
 // Get order history
-router.get('/history', authMiddleware, checkCustomerRole, async (req, res) => {
-  try {
-    const { user } = req;
-    const orderHistory = await confirmOrder.find({ user: user._id });
-
-    res.render('home-page/orderHistory', { orderHistory: orderHistory || [] });
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to retrieve order history' });
-  }
-});
+router.get('/history', authMiddleware, checkCustomerRole,submitOrder)
 
 
 
