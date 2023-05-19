1d4f734e96d2bb8c9b711d4278b9950c0933d21a
Do Le Long An <85084360+LaansDole@users.noreply.github.com>
2023-05-19 10:52:46 +0000

    Update UX aspect

--- public/js/validateVendor.js
+++ public/js/validateVendor.js
@@ -1,34 +1,22 @@
-// validate.js
+function validateInput(inputName, errorId) {
+  const inputElement = document.querySelector(`input[name="${inputName}"]`);
+  const errorElement = document.querySelector(`#${errorId}`);
 
-function validateNameVendor() {
-    const nameInput = document.querySelector('input[name="businessName"]');
-    const nameError = document.querySelector('#nameError');
-  
-    const name = nameInput.value;
-    if (name.length < 5) {
-      nameError.textContent = 'Must be at least 5 characters long.';
-      nameError.style.display = 'block';
-    } else {
-      nameError.style.display = 'none';
-    }
+  const inputValue = inputElement.value;
+  if (inputValue.length < 5) {
+    errorElement.textContent = 'Must be at least 5 characters long.';
+    errorElement.style.display = 'block';
+  } else {
+    errorElement.style.display = 'none';
   }
-  
-  function validateAddressVendor() {
-    const addressInput = document.querySelector('input[name="businessAddress"]');
-    const addressError = document.querySelector('#addressError');
-  
-    const address = addressInput.value;
-    if (address.length < 5) {
-      addressError.textContent = 'Must be at least 5 characters long.';
-      addressError.style.display = 'block';
-    } else {
-      addressError.style.display = 'none';
-    }
-  }
-  
-  
-  // Event listeners for input changes
-  document.querySelector('input[name="businessName"]').addEventListener('input', validateNameVendor);
-  document.querySelector('input[name="businessAddress"]').addEventListener('input', validateAddressVendor);
+}
+
+document.querySelector('input[name="businessName"]').addEventListener('input', () => {
+  validateInput('businessName', 'nameErrorVendor');
+});
+
+document.querySelector('input[name="businessAddress"]').addEventListener('input', () => {
+  validateInput('businessAddress', 'addressErrorVendor');
+});
   
   
